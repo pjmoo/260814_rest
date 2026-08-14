bac2eb2e9bb74232d331e7dc0203f6f508fd349f
pjmoo <00pjmoooo@gmail.com>
2026-08-14 11:33:21 +0900

    refactor: wrap BoardController responses with ResponseEntity and appropriate HTTP status codes

--- src/main/java/org/example/rest/controller/BoardController.java
+++ src/main/java/org/example/rest/controller/BoardController.java
@@ -5,6 +5,8 @@ import org.example.rest.domain.entity.BoardEntity;
 import org.example.rest.dto.BoardRequestDTO;
 import org.example.rest.dto.BoardResponseDTO;
 import org.example.rest.service.BoardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,46 +24,93 @@ public class BoardController {
 //        return "redirect:/";
 //    }
 //    @ResponseBody // view resolver를 통하지 않고 직접 json 등의 형태로 데이터를 줌
-    public BoardResponseDTO create(@RequestBody BoardRequestDTO boardRequestDTO) {
+//    public BoardResponseDTO create(@RequestBody BoardRequestDTO boardRequestDTO) {
+    public ResponseEntity<BoardResponseDTO> create(@RequestBody BoardRequestDTO boardRequestDTO) {
         BoardEntity boardEntity = boardRequestDTO.toEntity();
         BoardEntity saved = boardService.create(boardEntity);
-        return BoardResponseDTO.fromEntity(saved);
+//        return BoardResponseDTO.fromEntity(saved);
+//        return ResponseEntity.created() // 201 URI?
+        return ResponseEntity
+//                .status(201)
+                .status(HttpStatus.CREATED) // 201
+                .body(BoardResponseDTO.fromEntity(saved));
     }
+
+//    @GetMapping
+//    public List<BoardResponseDTO> readAll() {
+//        List<BoardEntity> boards = boardService.readAll();
+//        return boards.stream()
+//                .map(BoardResponseDTO::fromEntity)
+//                .toList();
+//    }
 
     @GetMapping
-    public List<BoardResponseDTO> readAll() {
+    public ResponseEntity<List<BoardResponseDTO>> readAll() {
         List<BoardEntity> boards = boardService.readAll();
-        return boards.stream()
-                .map(BoardResponseDTO::fromEntity)
-                .toList();
+
+//        return ResponseEntity.status(HttpStatus.OK) // 200
+        return ResponseEntity
+                .ok(boards.stream()
+                        .map(BoardResponseDTO::fromEntity)
+                        .toList());
     }
+
+//    @GetMapping("/{uuid}")
+//    // long의 main id를 받아도 상관없는데... ResponseDTO를 통해서 uuid만 노출하기로 (이 실습에선 결정)
+//    public BoardResponseDTO readOne(@PathVariable UUID uuid) {
+//        BoardEntity boardEntity = boardService.readOne(uuid);
+//        return BoardResponseDTO.fromEntity(boardEntity);
+//    }
 
     @GetMapping("/{uuid}")
-    // long의 main id를 받아도 상관없는데... ResponseDTO를 통해서 uuid만 노출하기로 (이 실습에선 결정)
-    public BoardResponseDTO readOne(@PathVariable UUID uuid) {
+    public ResponseEntity<BoardResponseDTO> readOne(@PathVariable UUID uuid) {
         BoardEntity boardEntity = boardService.readOne(uuid);
-        return BoardResponseDTO.fromEntity(boardEntity);
+        return ResponseEntity.ok(BoardResponseDTO.fromEntity(boardEntity));
     }
 
+//    @PutMapping("/{uuid}")
+//    public BoardResponseDTO update(
+//            @PathVariable UUID uuid,
+//            @RequestBody BoardRequestDTO boardRequestDTO) {
+//        // title, content를 한 번에 수정해줄 수 있는 형태
+//        BoardEntity updated = boardService.update(uuid, boardRequestDTO.toEntity());
+//        return BoardResponseDTO.fromEntity(updated);
+//    }
+
+//    @PatchMapping("/{uuid}/title")
+//    public BoardResponseDTO updateTitle(
+//            @PathVariable UUID uuid,
+//            @RequestParam String title) {
+//        BoardEntity updated = boardService.updateTitle(uuid, title);
+//        return BoardResponseDTO.fromEntity(updated);
+//    }
+
+
     @PutMapping("/{uuid}")
-    public BoardResponseDTO update(
+    public ResponseEntity<BoardResponseDTO> update(
             @PathVariable UUID uuid,
             @RequestBody BoardRequestDTO boardRequestDTO) {
-        // title, content를 한 번에 수정해줄 수 있는 형태
         BoardEntity updated = boardService.update(uuid, boardRequestDTO.toEntity());
-        return BoardResponseDTO.fromEntity(updated);
+        return ResponseEntity.ok(BoardResponseDTO.fromEntity(updated));
     }
 
     @PatchMapping("/{uuid}/title")
-    public BoardResponseDTO updateTitle(
+    public ResponseEntity<BoardResponseDTO> updateTitle(
             @PathVariable UUID uuid,
             @RequestParam String title) {
         BoardEntity updated = boardService.updateTitle(uuid, title);
-        return BoardResponseDTO.fromEntity(updated);
+        return ResponseEntity.ok(BoardResponseDTO.fromEntity(updated));
     }
 
+//    @DeleteMapping("/{uuid}")
+//    public void delete(@PathVariable UUID uuid) {
+//        boardService.delete(uuid);
+//    }
+
     @DeleteMapping("/{uuid}")
-    public void delete(@PathVariable UUID uuid) {
+    // Generic으로 표현해야하므로 비어있다는 void X. Wrapper Void
+    public ResponseEntity<Void> delete(@PathVariable UUID uuid) {
         boardService.delete(uuid);
+        return ResponseEntity.noContent().build();
     }
 }
